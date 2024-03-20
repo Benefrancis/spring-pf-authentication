b3ce45faae8841485aa0febdf30a0d38870c1d08
Benefrancis <benefrancis@gmail.com>
2024-03-20 11:55:25 -0300

    CODE 201

--- src/main/java/br/com/fiap/springpfauthentication/resource/UsuarioResource.java
+++ src/main/java/br/com/fiap/springpfauthentication/resource/UsuarioResource.java
@@ -8,9 +8,13 @@ import br.com.fiap.springpfauthentication.entity.Usuario;
 import br.com.fiap.springpfauthentication.repository.UsuarioRepository;
 import br.com.fiap.springpfauthentication.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Objects;
 
@@ -38,9 +42,32 @@ public class UsuarioResource {
 
     @Transactional
     @PostMapping
-    public UsuarioResponse save(@RequestBody UsuarioRequest u) {
+    public ResponseEntity<UsuarioResponse> save(@RequestBody UsuarioRequest u) {
+
+        //Se não me informou a propriedade pessoa na hora de mandar a requisição eu retorno null
         if (Objects.isNull(u.pessoa())) return null;
-        return service.toResponse(repo.save(service.toEntity(u)));
+
+        //Eu recebi um DTO (Usuariorequest), entretanto eu só posso salvar Entidades, por isso eu converti
+        Usuario entity = service.toEntity(u);
+
+        //Agora posso salvar no meu repositório.
+        Usuario salvo = repo.save(entity);
+
+        //Neste momento eu vou transformar o entity que recebeu o Id do Banco de Dados em um DTO de resposta
+        UsuarioResponse body = service.toResponse(salvo);
+
+
+        //Agora minha preocupação está em criar o correto cabeçalho de resposta (Response),
+        // quando salvamos, é necessário incluir no cabeçalho o Location do recurso que foi salvo.
+        // Neste caso o resource é de usuário
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(salvo.getId())
+                .toUri();
+
+       return ResponseEntity.created(uri).body(body);
+
     }
 
 
